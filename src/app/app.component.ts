bd6392005c60e6c099419267766eb9a7cbbb1f17
somai hamza <HAMZASOMAI@outlook.com>
2025-12-29 12:50:39 +0100

    Add project structure : header & footer & product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddProductComponent } from "./views/products/views/add-product/add-product/add-product.component";
+import { ListProductsComponent } from "./views/products/views/list-products/list-products.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddProductComponent, ListProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
